56420794caa677bab8c3e1cd017f7646badab45d
aydachettiet <dantetran04@gmail.com>
2023-10-29 22:31:54 +0700

    nam update thongke

--- src/main/java/com/phanlop/khoahoc/Controller/ThongkeController.java
+++ src/main/java/com/phanlop/khoahoc/Controller/ThongkeController.java
@@ -3,6 +3,11 @@ package com.phanlop.khoahoc.Controller;
 import com.phanlop.khoahoc.DTO.ThongkeCourseDTO;
 import com.phanlop.khoahoc.Entity.*;
 
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -48,6 +53,7 @@ public class ThongkeController {
             }
             Course temp=course;
             temp.setGia(temp.getGia()*count);
+            temp.setState(count);
             coursetop3.add(temp);
         }
         Collections.sort(coursetop3, Comparator.comparingDouble(Course::getGia).reversed());
@@ -62,6 +68,69 @@ public class ThongkeController {
         model.addAttribute("teacherid", teacherid);
         return "thongke";
     }
+    @GetMapping("/thongkedate")
+    public String thongkedate(@RequestParam("teacherid") long teacherid,
+                        @RequestParam("startDate") Date startDate,
+                        @RequestParam("endDate") Date endDate,Model model){
+        User user=userServices.getUserById(teacherid);
+        List<Course> courses=courseServices.findCourseOfTeacher(user);
+        List<Enrollment> enrollments=enrollmentServices.getAll();
+        String staString=String.valueOf(startDate);
+        String endString=String.valueOf(endDate);
+        LocalDate startLocal=LocalDate.parse(staString, DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate endLocal=LocalDate.parse(endString,DateTimeFormatter.ISO_LOCAL_DATE);
+
+
+        List<Course> teacherCourseHaveBeenBuy=new ArrayList<>();
+        ZoneId zoneId2 = ZoneId.of("Asia/Ho_Chi_Minh");  
+        int sum=0;
+        List<ThongkeCourseDTO> coursefix=new ArrayList<>();
+        for (Course course : courses) {
+            int count=0;
+            LocalDate local=null;
+            for (Enrollment enrollment : enrollments) {
+                if(course.getCourseID()==enrollment.getCourse().getCourseID()){
+                    local=LocalDate.ofInstant(enrollment.getDateJoined(),zoneId2);
+                    if(local.isAfter(startLocal)&& local.isBefore(endLocal)){
+                        count++;
+                    }
+                }
+            }
+            ThongkeCourseDTO coursedto=new ThongkeCourseDTO();
+            coursedto.courseName=course.getCourseName();
+            coursedto.gia=course.getGia();
+            coursedto.courseid=course.getCourseID();
+            coursedto.courseRevenue=course.getGia()*count;
+            sum+=coursedto.courseRevenue;
+            coursedto.courseBuyCount=count;
+            coursedto.avatar=course.getCourseAvt();
+            coursefix.add(coursedto);
+        }
+       
+        int flag=1; 
+        model.addAttribute("tongdoanhthu", sum);
+        model.addAttribute("flag", flag);
+        model.addAttribute("teacherid", teacherid);
+        model.addAttribute("courses", coursefix);
+        return "thongke";
+    }
+    @GetMapping("/thongke1")
+    public String thongke1(@RequestParam("teacherid") long teacherid,Model model){
+        User user=userServices.getUserById(teacherid);
+        
+        int flag=1;
+        int tongdoanhthu=0;
+        List<Course> courses=new ArrayList<>();
+        model.addAttribute("tongdoanhthu", tongdoanhthu);
+        model.addAttribute("courses", courses);
+        model.addAttribute("flag", flag);
+        model.addAttribute("teacherid", teacherid);
+        return "thongke";
+    }
+
+
+
+
     @GetMapping("/thongke2")
     public String thongke2(@RequestParam("teacherid") long teacherid,Model model){
         User user=userServices.getUserById(teacherid);
